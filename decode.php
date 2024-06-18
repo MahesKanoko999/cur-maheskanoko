f6c9f74afe6310a7e9874968aca5981e0c90d134
MahesKanoko999 <mahesyt@gmail.com>
2024-06-18 20:23:13 +0800

    update decode

--- decode.php
+++ decode.php
@@ -12,7 +12,6 @@ $con = new mysqli($host_name, $user_name, $password, $database, $port);
 if ($con->connect_error) {
     die("Connection failed: " . $con->connect_error);
 }
-
 ?>
 
 <h2><b>Request</b></h2>
@@ -27,7 +26,7 @@ function http_request($url)
 {
     $ch = curl_init(); 
     curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
+    curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
     $output = curl_exec($ch); 
     curl_close($ch);      
@@ -35,42 +34,33 @@ function http_request($url)
 }
 
 $api_profile = http_request("https://raw.githubusercontent.com/MahesKanoko999/curl-maheskanoko/main/api/encode_mk.json"); 
-
 $profile = json_decode($api_profile, TRUE);
 
-$id     = array_column($profile, 'id');
-$pelanggan_id    = array_column($profile, 'pelanggan_id');
-$tanggal   = array_column($profile, 'tanggal');
-$menu_id  = array_column($profile, 'menu_id');
-$jumlah  = array_column($profile, 'jumlah');
-$total_harga  = array_column($profile, 'total_harga');
-$handle_karyawan  = array_column($profile, 'handle_karyawan');
-$last   = count($id);
+foreach ($profile as $data) {
+    $id = $data['id'];
+    $pelanggan_id = $data['pelanggan_id'];
+    $tanggal = $data['tanggal'];
+    $menu_id = $data['menu_id'];
+    $jumlah = $data['jumlah'];
+    $total_harga = $data['total_harga'];
+    $handle_karyawan = $data['handle_karyawan'];
 
-for ($x = 0; $x < $last; $x++) 
-{
-    // Use prepared statements to prevent SQL injection
     $stmt = $con->prepare("SELECT * FROM tb_detail_pesanan WHERE id = ?");
-    $stmt->bind_param("s", $id[$x]);
+    $stmt->bind_param("s", $id);
     $stmt->execute();
-    $result = $stmt->get_result();
-    $dt = $result->num_rows;
+    $stmt->store_result();
+    $dt = $stmt->num_rows;
     $stmt->close();
 
-    if ($dt == 0)
-    {
+    if ($dt == 0) {
         $stmt = $con->prepare("INSERT INTO tb_detail_pesanan (id, pelanggan_id, tanggal, menu_id, jumlah, total_harga, handle_karyawan) VALUES (?, ?, ?, ?, ?, ?, ?)");
-        $stmt->bind_param("sssssss", $id[$x], $pelanggan_id[$x], $tanggal[$x], $menu_id[$x], $jumlah[$x], $total_harga[$x], $handle_karyawan[$x]);
+        $stmt->bind_param("sssssss", $id, $pelanggan_id, $tanggal, $menu_id, $jumlah, $total_harga, $handle_karyawan);
         $stmt->execute();
         $stmt->close();
-        
-        echo $id[$x] . " " . $pelanggan_id[$x] . " " .$tanggal[$x]. " " .$menu_id[$x]. " " .$jumlah[$x]. " " .$total_harga[$x]. " " .$handle_karyawan[$x] . "<br>";
-    }
-    else
-    {
-        // Update query using prepared statements
+        echo "$id $pelanggan_id $tanggal $menu_id $jumlah $total_harga $handle_karyawan<br>";
+    } else {
         $stmt = $con->prepare("UPDATE tb_detail_pesanan SET pelanggan_id=?, tanggal=?, menu_id=?, jumlah=?, total_harga=?, handle_karyawan=? WHERE id=?");
-        $stmt->bind_param("sssssss", $pelanggan_id[$x], $tanggal[$x], $menu_id[$x], $jumlah[$x], $total_harga[$x], $handle_karyawan[$x], $id[$x]);
+        $stmt->bind_param("sssssss", $pelanggan_id, $tanggal, $menu_id, $jumlah, $total_harga, $handle_karyawan, $id);
         $stmt->execute();
         $stmt->close();
     }
